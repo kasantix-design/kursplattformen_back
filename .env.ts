620420aeb0bae42cdf673654d409f33c48497718
kasantix-design <kasantix@gmail.com>
2025-11-15 19:00:50 +0100

    Update .env.ts

--- .env.ts
+++ .env.ts
@@ -5,5 +5,16 @@ FRONTEND_URL=https://kursplattformen.netlify.app
 STRIPE_KEY=test
 GMAIL_USER=kasantix@gmail.com
 GMAIL_PASS=cgzf pllp bsgr ionm
-GMAIL_USER: process.env.GMAIL_USER as string,
-GMAIL_PASS: process.env.GMAIL_PASS as string,
+export const env = {
+  NODE_ENV: process.env.NODE_ENV!,
+  PORT: Number(process.env.PORT),
+  MONGODB_URI: process.env.MONGODB_URI!,
+  JWT_SECRET: process.env.JWT_SECRET!,
+  FRONTEND_URL: process.env.FRONTEND_URL!,
+  STRIPE_KEY: process.env.STRIPE_KEY!,
+  OUTLOOK_EMAIL: process.env.OUTLOOK_EMAIL!,
+  OUTLOOK_PASS: process.env.OUTLOOK_PASS!,
+  GMAIL_USER: process.env.GMAIL_USER!,
+  GMAIL_PASS: process.env.GMAIL_PASS!,
+};
+
