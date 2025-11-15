489e590a1858a6e316ce2c6fc47f21d81ec1bdc3
kasantix-design <kasantix@gmail.com>
2025-11-15 18:52:00 +0100

    Update medlemController.ts

--- src/controllers/medlemController.ts
+++ src/controllers/medlemController.ts
@@ -1,7 +1,51 @@
-import { Request, Response } from "express"
-import { Bruker } from "../models/Bruker"
+import { Request, Response } from "express";
+import Medlem from "../models/Medlem";
 
-export const hentProfil = async (req: Request, res: Response) => {
-  const bruker = await Bruker.findById(req.user.id).select("-passord")
-  res.json(bruker)
-}
+// Hent info om innlogget medlem
+export const hentMinProfil = async (req: Request, res: Response) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Ikke autentisert" });
+  }
+
+  try {
+    const medlem = await Medlem.findById(req.user.id).select("-passord");
+    if (!medlem) {
+      return res.status(404).json({ message: "Medlem ikke funnet" });
+    }
+
+    res.status(200).json(medlem);
+  } catch (err) {
+    res.status(500).json({ message: "Feil ved henting av medlemsprofil" });
+  }
+};
+
+// Oppdater medlemsinfo
+export const oppdaterProfil = async (req: Request, res: Response) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Ikke autorisert" });
+  }
+
+  try {
+    const oppdatert = await Medlem.findByIdAndUpdate(req.user.id, req.body, {
+      new: true,
+    }).select("-passord");
+
+    res.status(200).json(oppdatert);
+  } catch (err) {
+    res.status(500).json({ message: "Kunne ikke oppdatere profil" });
+  }
+};
+
+// Slett medlemskonto
+export const slettKonto = async (req: Request, res: Response) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Ikke autorisert" });
+  }
+
+  try {
+    await Medlem.findByIdAndDelete(req.user.id);
+    res.status(200).json({ message: "Konto slettet" });
+  } catch (err) {
+    res.status(500).json({ message: "Feil ved sletting av konto" });
+  }
+};
