c849da91e56b59a1138730f047404f8770ea6d9b
kasantix-design <kasantix@gmail.com>
2025-11-15 18:46:23 +0100

    Update bloggController.ts

--- src/controllers/bloggController.ts
+++ src/controllers/bloggController.ts
@@ -1,12 +1,50 @@
-import { Request, Response } from "express"
-import { Blogg } from "../models/Blogg"
+import { Request, Response } from "express";
+import Blogg from "../models/Blogg";
 
-export const hentAlleBlogg = async (_: Request, res: Response) => {
-  const innlegg = await Blogg.find().sort({ opprettet: -1 })
-  res.json(innlegg)
-}
+// Hent alle innlegg
+export const hentAlleInnlegg = async (req: Request, res: Response) => {
+  try {
+    const innlegg = await Blogg.find().populate("forfatter", "navn");
+    res.status(200).json(innlegg);
+  } catch (err) {
+    res.status(500).json({ message: "Feil ved henting av innlegg" });
+  }
+};
 
-export const opprettBlogg = async (req: Request, res: Response) => {
-  const nytt = await Blogg.create({ ...req.body, forfatter: req.user.id })
-  res.status(201).json(nytt)
-}
+// Opprett nytt innlegg
+export const opprettInnlegg = async (req: Request, res: Response) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Ikke autentisert bruker" });
+  }
+
+  const { tittel, innhold } = req.body;
+
+  try {
+    const nyttInnlegg = new Blogg({
+      tittel,
+      innhold,
+      forfatter: req.user.id,
+    });
+
+    await nyttInnlegg.save();
+    res.status(201).json(nyttInnlegg);
+  } catch (err) {
+    res.status(500).json({ message: "Kunne ikke opprette innlegg" });
+  }
+};
+
+// Slett et innlegg
+export const slettInnlegg = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const slettet = await Blogg.findByIdAndDelete(id);
+    if (!slettet) {
+      return res.status(404).json({ message: "Innlegg ikke funnet" });
+    }
+    res.status(200).json({ message: "Innlegg slettet" });
+  } catch (err) {
+    res.status(500).json({ message: "Feil ved sletting" });
+  }
+};
+
