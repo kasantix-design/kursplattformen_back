5a5266442417ac95eba400a47a9efb4bf03535e2
kasantix-design <kasantix@gmail.com>
2025-11-15 19:00:21 +0100

    Update .env.ts

--- .env.ts
+++ .env.ts
@@ -5,3 +5,5 @@ FRONTEND_URL=https://kursplattformen.netlify.app
 STRIPE_KEY=test
 GMAIL_USER=kasantix@gmail.com
 GMAIL_PASS=cgzf pllp bsgr ionm
+GMAIL_USER: process.env.GMAIL_USER as string,
+GMAIL_PASS: process.env.GMAIL_PASS as string,
