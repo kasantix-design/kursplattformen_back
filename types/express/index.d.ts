22c6a72dbdf56ddc122faabdb90180cf8761c0f2
kasantix-design <kasantix@gmail.com>
2025-11-16 22:21:32 +0100

    Refactor UserPayload and Request interfaces in Express

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -1,25 +1,13 @@
-import { ObjectId } from "mongoose"
+import { ObjectId } from 'mongoose'
 
-declare global {
-  namespace Express {
-    interface UserPayload {
-      id: string | ObjectId
-      rolle: "medlem" | "admin"
-    }
-
-    interface Request {
-      user?: UserPayload
-    }
-  }
-}
-
-export {}
 declare module 'express' {
+  interface UserPayload {
+    id: string | ObjectId
+    email?: string
+    rolle: 'medlem' | 'admin'
+  }
+
   interface Request {
-    user?: {
-      id: string
-      email?: string
-      rolle?: string
-    }
+    user?: UserPayload
   }
 }
