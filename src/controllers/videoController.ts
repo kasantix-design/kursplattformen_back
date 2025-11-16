3281d1e72c6be64c0b80e98167bcdbb1ee4571ed
kasantix-design <kasantix@gmail.com>
2025-11-16 10:44:15 +0100

    Update videoController.ts

--- src/controllers/videoController.ts
+++ src/controllers/videoController.ts
@@ -1,11 +1,8 @@
 import { Request, Response } from "express";
-import Video from "../models/Video";
+import { Video } from "../models/Video";
 
-// Last opp referanse til video (Jitsi/Jibri-opptak lenke)
-export const lastOppVideo = async (req: Request, res: Response) => {
-  if (!req.user) {
-    return res.status(401).json({ message: "Ikke autorisert" });
-  }
+export const lagreVideo = async (req: Request, res: Response) => {
+  if (!req.user) return res.status(401).json({ message: "Ikke autentisert" });
 
   const { tittel, url } = req.body;
 
@@ -19,15 +16,12 @@ export const lastOppVideo = async (req: Request, res: Response) => {
     await nyVideo.save();
     res.status(201).json(nyVideo);
   } catch (err) {
-    res.status(500).json({ message: "Kunne ikke laste opp video" });
+    res.status(500).json({ message: "Kunne ikke lagre video" });
   }
 };
 
-// Hent videoer for bruker
-export const hentMineVideoer = async (req: Request, res: Response) => {
-  if (!req.user) {
-    return res.status(401).json({ message: "Ikke autorisert" });
-  }
+export const hentVideoer = async (req: Request, res: Response) => {
+  if (!req.user) return res.status(401).json({ message: "Ikke autorisert" });
 
   try {
     const videoer = await Video.find({ bruker: req.user.id });
