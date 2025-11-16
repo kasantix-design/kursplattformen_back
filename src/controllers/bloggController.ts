b7b1bb62ac411762c50ff97342e457ba7a841889
kasantix-design <kasantix@gmail.com>
2025-11-16 10:43:36 +0100

    Update bloggController.ts

--- src/controllers/bloggController.ts
+++ src/controllers/bloggController.ts
@@ -1,55 +1,30 @@
 import { Request, Response } from "express";
-import Blogg from "../models/Blogg";
+import { Blogg } from "../models/Blogg";
 
-// Hent alle innlegg
-export const hentAlleInnlegg = async (req: Request, res: Response) => {
+export const hentAlleBlogg = async (req: Request, res: Response) => {
   try {
-    const innlegg = await Blogg.find().populate("forfatter", "navn");
-    res.status(200).json(innlegg);
+    const blogger = await Blogg.find();
+    res.status(200).json(blogger);
   } catch (err) {
-    res.status(500).json({ message: "Feil ved henting av innlegg" });
+    res.status(500).json({ message: "Feil ved henting av blogginnlegg" });
   }
 };
 
-// Opprett nytt innlegg
-export const hentMinSide = async (req: Request, res: Response) => {
-  if (!req.user) {
-    return res.status(401).json({ message: "Ikke autentisert" });
-  }
-export const opprettInnlegg = async (req: Request, res: Response) => {
-  if (!req.user) {
-    return res.status(401).json({ message: "Ikke autentisert bruker" });
-  }
+export const opprettBlogg = async (req: Request, res: Response) => {
+  if (!req.user) return res.status(401).json({ message: "Ikke autentisert" });
 
   const { tittel, innhold } = req.body;
 
   try {
-    const nyttInnlegg = new Blogg({
+    const nyBlogg = new Blogg({
       tittel,
       innhold,
       forfatter: req.user.id,
     });
 
-    await nyttInnlegg.save();
-    res.status(201).json(nyttInnlegg);
+    await nyBlogg.save();
+    res.status(201).json(nyBlogg);
   } catch (err) {
-    res.status(500).json({ message: "Kunne ikke opprette innlegg" });
+    res.status(500).json({ message: "Kunne ikke opprette blogginnlegg" });
   }
 };
-
-// Slett et innlegg
-export const slettInnlegg = async (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  try {
-    const slettet = await Blogg.findByIdAndDelete(id);
-    if (!slettet) {
-      return res.status(404).json({ message: "Innlegg ikke funnet" });
-    }
-    res.status(200).json({ message: "Innlegg slettet" });
-  } catch (err) {
-    res.status(500).json({ message: "Feil ved sletting" });
-  }
-};
-  }
-
