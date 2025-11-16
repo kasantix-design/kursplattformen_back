d70426a0a739af9f72f3cccf2af3f328bfe776f6
kasantix-design <kasantix@gmail.com>
2025-11-16 17:49:01 +0100

    Update authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,9 @@
-import express from "express";
-import { login, register } from "../controllers/authController";
+import express from "express"
+import { register, login } from "../controllers/authController"
 
-const router = express.Router();
+const router = express.Router()
 
-router.post("/login", login);
-router.post("/register", register);
+router.post("/register", register)
+router.post("/login", login)
 
-export default router;
+export default router
