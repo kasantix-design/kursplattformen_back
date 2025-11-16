0fad0320b869af517d0b279ebfe567b09419bb3b
kasantix-design <kasantix@gmail.com>
2025-11-16 18:08:57 +0100

    Update .env.ts

--- .env.ts
+++ .env.ts
@@ -1,8 +1,8 @@
 PORT=4000
-MONGODB_URI=test
+MONGODB_URI=wxSylpXNWPqHXuTO
 JWT_SECRET=Lamaskenfalle
 FRONTEND_URL=https://kursplattformen.netlify.app
-STRIPE_KEY=test
+STRIPE_KEY=sk_test_51STACSK93dHJoD6xGHJnwqCIUkjhZdltqN8K74mbfGzAfL5ed8kLOSqKlQvtUXjqcUTQaLl4DKsX3tmnlcBDe7C200JkPNUHXd
 GMAIL_USER=kasantix@gmail.com
 GMAIL_PASS=cgzf pllp bsgr ionm
 export const env = {
@@ -12,8 +12,6 @@ export const env = {
   JWT_SECRET: process.env.JWT_SECRET!,
   FRONTEND_URL: process.env.FRONTEND_URL!,
   STRIPE_KEY: process.env.STRIPE_KEY!,
-  OUTLOOK_EMAIL: process.env.OUTLOOK_EMAIL!,
-  OUTLOOK_PASS: process.env.OUTLOOK_PASS!,
   GMAIL_USER: process.env.GMAIL_USER!,
   GMAIL_PASS: process.env.GMAIL_PASS!,
 };
