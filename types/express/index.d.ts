899e037cefa3a7284d0e36dae6614d69cbfd9a2c
kasantix-design <kasantix@gmail.com>
2025-11-16 11:46:32 +0100

    Update index.d.ts

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -1,14 +1,31 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: "development" | "production";
-    PORT: string;
-    MONGODB_URI: string;
-    JWT_SECRET: string;
-    FRONTEND_URL: string;
-    STRIPE_KEY: string;
-    OUTLOOK_EMAIL: string;
-    OUTLOOK_PASS: string;
-    GMAIL_USER: string;        // 
-    GMAIL_PASS: string;        // 
+import { ObjectId } from "mongoose";
+
+declare global {
+  namespace Express {
+    interface UserPayload {
+      id: string | ObjectId;
+      rolle: string;
+    }
+
+    interface Request {
+      user?: UserPayload;
+    }
+  }
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production";
+      PORT: string;
+      MONGODB_URI: string;
+      JWT_SECRET: string;
+      FRONTEND_URL: string;
+      STRIPE_KEY: string;
+      OUTLOOK_EMAIL: string;
+      OUTLOOK_PASS: string;
+      GMAIL_USER: string;
+      GMAIL_PASS: string;
+    }
   }
 }
+
+export {};
