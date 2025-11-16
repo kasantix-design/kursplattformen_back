8ca76e58232659cfa406e95ef0d67fded3769c93
kasantix-design <kasantix@gmail.com>
2025-11-16 22:57:51 +0100

    Refactor server startup to use default PORT

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
-import { connectDB } from "./config/db"
-import { env } from "./config/env"
-import app from "./app"
+import { connectDB } from './config/db'
+import { env } from './config/env'
+import app from './app'
 
-connectDB().then(() => {
-  app.listen(env.PORT, () => {
-    console.log(`🚀 Server kjører på http://localhost:${env.PORT}`)
+const PORT = env.PORT || 4000
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`🚀 Server kjører på http://localhost:${PORT}`)
+    })
   })
-})
+  .catch((err) => {
+    console.error('❌ Klarte ikke å koble til databasen:', err)
+    process.exit(1)
+  })
