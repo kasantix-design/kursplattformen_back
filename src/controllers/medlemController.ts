6cd1311e337b0c845d0a2301aad0fdca9a16e361
kasantix-design <kasantix@gmail.com>
2025-11-16 18:57:09 +0100

    Update medlemController.ts

--- src/controllers/medlemController.ts
+++ src/controllers/medlemController.ts
@@ -1,58 +1,52 @@
 import { Request, Response } from "express";
 import { Medlem } from "../models/Medlem";
 
-
-
-// Hent info om innlogget medlem
-export const hentMinSide = async (req: Request, res: Response) => {
-  if (!req.user) {
-    return res.status(401).json({ message: "Ikke autentisert" });
-  }
-export const hentMinProfil = async (req: Request, res: Response) => {
+// Hent innlogget medlemsprofil
+export const hentProfil = async (req: Request, res: Response) => {
   if (!req.user) {
     return res.status(401).json({ message: "Ikke autentisert" });
   }
 
   try {
-    const medlem = await Medlem.findById(req.user.id).select("-passord");
+    const medlem = await Medlem.findById(req.user.id).select("-passord"); // Fjern passord fra respons
     if (!medlem) {
       return res.status(404).json({ message: "Medlem ikke funnet" });
     }
-
     res.status(200).json(medlem);
-  } catch (err) {
-    res.status(500).json({ message: "Feil ved henting av medlemsprofil" });
+  } catch (error) {
+    res.status(500).json({ message: "Serverfeil" });
   }
 };
 
-// Oppdater medlemsinfo
+// Oppdater medlemsprofil
 export const oppdaterProfil = async (req: Request, res: Response) => {
   if (!req.user) {
-    return res.status(401).json({ message: "Ikke autorisert" });
+    return res.status(401).json({ message: "Ikke autentisert" });
   }
 
   try {
-    const oppdatert = await Medlem.findByIdAndUpdate(req.user.id, req.body, {
+    const oppdatertMedlem = await Medlem.findByIdAndUpdate(req.user.id, req.body, {
       new: true,
+      runValidators: true,
     }).select("-passord");
 
-    res.status(200).json(oppdatert);
-  } catch (err) {
+    res.status(200).json(oppdatertMedlem);
+  } catch (error) {
     res.status(500).json({ message: "Kunne ikke oppdatere profil" });
   }
 };
 
 // Slett medlemskonto
-export const slettKonto = async (req: Request, res: Response) => {
+export const slettProfil = async (req: Request, res: Response) => {
   if (!req.user) {
-    return res.status(401).json({ message: "Ikke autorisert" });
+    return res.status(401).json({ message: "Ikke autentisert" });
   }
 
   try {
     await Medlem.findByIdAndDelete(req.user.id);
     res.status(200).json({ message: "Konto slettet" });
-  } catch (err) {
-    res.status(500).json({ message: "Feil ved sletting av konto" });
+  } catch (error) {
+    res.status(500).json({ message: "Kunne ikke slette konto" });
   }
-  };
-}
+};
+
