3b1409aad07d040a85269e21c1bccc4731998d30
kasantix-design <kasantix@gmail.com>
2025-11-16 19:00:00 +0100

    Update Medlem.ts

--- src/models/Medlem.ts
+++ src/models/Medlem.ts
@@ -1,17 +1,20 @@
-import mongoose, { Document, Schema } from "mongoose"
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IMedlem extends Document {
-  navn: string
-  epost: string
-  passord: string
-  rolle: "medlem" | "admin"
+  navn: string;
+  epost: string;
+  passord: string;
+  rolle: "admin" | "medlem";
 }
 
-const medlemSchema = new Schema<IMedlem>({
-  navn: { type: String, required: true },
-  epost: { type: String, required: true, unique: true },
-  passord: { type: String, required: true },
-  rolle: { type: String, enum: ["medlem", "admin"], default: "medlem" }
-})
+const MedlemSchema: Schema = new Schema(
+  {
+    navn: { type: String, required: true },
+    epost: { type: String, required: true, unique: true },
+    passord: { type: String, required: true },
+    rolle: { type: String, enum: ["admin", "medlem"], default: "medlem" },
+  },
+  { timestamps: true }
+);
 
-export const Medlem = mongoose.model<IMedlem>("Medlem", medlemSchema)
+export const Medlem = mongoose.model<IMedlem>("Medlem", MedlemSchema);
