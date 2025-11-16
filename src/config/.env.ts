9a52e1dcd30ef0b23d416f3b447f378038875d62
kasantix-design <kasantix@gmail.com>
2025-11-16 22:03:29 +0100

    Refactor environment variable configuration
    Updated environment variable handling to use non-null assertions and removed default values.

--- src/config/.env.ts
+++ src/config/.env.ts
@@ -1,11 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export const env = {
-  NODE_ENV: process.env.NODE_ENV || "development",
-  PORT: Number(process.env.PORT) || 4000,
-  MONGODB_URI: process.env.MONGODB_URI || "",
-  JWT_SECRET: process.env.JWT_SECRET || "Lamaskenfalle",
-  FRONTEND_URL: process.env.FRONTEND_URL || "",
-  STRIPE_KEY: process.env.STRIPE_KEY || "",
-  GMAIL_USER: process.env.GMAIL_USER || "kasantix@gmail.com",
-  GMAIL_PASS: process.env.GMAIL_PASS || "",
-  ADMIN_EMAIL: process.env.ADMIN_EMAIL || "kasantix@gmail.com",
-};
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  PORT: Number(process.env.PORT),
+  MONGODB_URI: process.env.MONGODB_URI!,
+  JWT_SECRET: process.env.JWT_SECRET!,
+  FRONTEND_URL: process.env.FRONTEND_URL!,
+  STRIPE_KEY: process.env.STRIPE_KEY!,
+  GMAIL_USER: process.env.GMAIL_USER!,
+  GMAIL_PASS: process.env.GMAIL_PASS!,
+}
