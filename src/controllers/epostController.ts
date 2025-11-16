f0d98814d4b466344747513feaefda4fcd0efa42
kasantix-design <kasantix@gmail.com>
2025-11-16 18:46:48 +0100

    Update epostController.ts

--- src/controllers/epostController.ts
+++ src/controllers/epostController.ts
@@ -8,6 +8,7 @@ export const kontaktAdmin = async (req: Request, res: Response) => {
     await sendEpost({ navn, epost, melding });
     res.status(200).json({ message: "E-post sendt!" });
   } catch (err) {
+    console.error("Feil ved sending av e-post:", err);
     res.status(500).json({ message: "Feil ved sending av e-post." });
   }
 };
